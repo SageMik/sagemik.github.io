ffcb36af84be9ff6ae2b88f4b0c0e08d08a93726
SageMik <sagemik@163.com>
2026-08-30 00:59:42 +0800

    关闭公告栏

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -48,16 +48,16 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 是否在文章详情页显示
 			showOnPostPage: true,
 		},
-		{
-			// 组件类型：公告组件
-			type: "announcement",
-			// 是否启用该组件
-			enable: true,
-			// 组件位置
-			position: "top",
-			// 是否在文章详情页显示
-			showOnPostPage: true,
-		},
+		// {
+		// 	// 组件类型：公告组件
+		// 	type: "announcement",
+		// 	// 是否启用该组件
+		// 	enable: true,
+		// 	// 组件位置
+		// 	position: "top",
+		// 	// 是否在文章详情页显示
+		// 	showOnPostPage: true,
+		// },
 		{
 			// 组件类型：音乐播放器
 			type: "music",
